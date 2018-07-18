2b0ecc49ab7d6c094d55b774171d0a1d3b5791a8
Nicoleyy <daidaiwxy@126.com>
2018-07-18 17:45:09 +0800

    Create Toggleable.vue

---
+++ Vue/Toggleable.vue
@@ -0,0 +1,24 @@
+<script>
+const prop = 'value'
+const event = 'input'
+export default {
+  name: 'Toggleable',
+  model: { prop, event },
+  props: {
+    [prop]: { required: false }
+  },
+  data () {
+    return {
+      isActive: !!this[prop]
+    }
+  },
+  watch: {
+    [prop] (val) {
+      this.isActive = !!val
+    },
+    isActive (val) {
+      !!val !== this[prop] && this.$emit(event, val)
+    }
+  }
+}
+</script>
